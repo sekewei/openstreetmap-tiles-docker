d5da57a1ca5489c107b248a821b4ac69b99b5231
Rex Tsai (蔡志展) <rex.cc.tsai@gmail.com>
2015-08-05 23:02:18 +0800

    ignore if postgresql is already inited.

--- run.sh
+++ run.sh
@@ -25,13 +25,14 @@ initdb () {
     echo "Initialising postgresql"
     if [ -d /var/lib/postgresql/9.3/main ] && [ $( ls -A /var/lib/postgresql/9.3/main | wc -c ) -ge 0 ]
     then
-        die "Initialisation failed: the directory is not empty: /var/lib/postgresql/9.3/main"
+        echo "Initialisation failed: the directory is not empty: /var/lib/postgresql/9.3/main"
+    else
+        mkdir -p /var/lib/postgresql/9.3/main && chown -R postgres /var/lib/postgresql/
+        sudo -u postgres -i /usr/lib/postgresql/9.3/bin/initdb --pgdata /var/lib/postgresql/9.3/main
+        ln -s /etc/ssl/certs/ssl-cert-snakeoil.pem /var/lib/postgresql/9.3/main/server.crt
+        ln -s /etc/ssl/private/ssl-cert-snakeoil.key /var/lib/postgresql/9.3/main/server.key
     fi
 
-    mkdir -p /var/lib/postgresql/9.3/main && chown -R postgres /var/lib/postgresql/
-    sudo -u postgres -i /usr/lib/postgresql/9.3/bin/initdb --pgdata /var/lib/postgresql/9.3/main
-    ln -s /etc/ssl/certs/ssl-cert-snakeoil.pem /var/lib/postgresql/9.3/main/server.crt
-    ln -s /etc/ssl/private/ssl-cert-snakeoil.key /var/lib/postgresql/9.3/main/server.key
 }
 
 createuser () {
